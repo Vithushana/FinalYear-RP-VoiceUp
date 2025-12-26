53cad4ebe1d915908cab2f09e209f227b0793834
Harishram Muhunthan <harish012801@gmail.com>
2025-12-26 17:02:36 +0530

    Remove all old files

--- Desktop/relevance_and_abuse_filteration_harish/application/android/app/src/main/kotlin/com/example/voice_up_ime/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.voice_up_ime
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Desktop/relevance_and_abuse_filteration_harish/application/run_flutter.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set FLUTTER_PATH=C:\Users\Admin pc\Downloads\flutter_windows_3.38.5-stable\flutter\bin
-"%FLUTTER_PATH%\flutter.bat" %*
